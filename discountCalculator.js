fd2e12692de3c89c744e474b62fa44827677b8ac
salmonsalad <56084008+salmonsalad@users.noreply.github.com>
2024-04-25 22:30:20 +0900

    Update discountCalculator.js

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,4 +1,5 @@
 function calculateDiscountedPrice(price, discountRate) {
+  // 입력값 유효성 검사
   if (typeof price !== 'number' || typeof discountRate !== 'number') {
     throw new Error('Price and discount rate must be numbers');
   }
@@ -7,10 +8,12 @@ function calculateDiscountedPrice(price, discountRate) {
     throw new Error('Invalid price or discount rate');
   }
 
+  // 할인 계산
   const discountAmount = (price * discountRate) / 100;
   const discountedPrice = price - discountAmount;
 
-  return discountedPrice;
+  // 소수점 둘째 자리까지 반올림하여 반환
+  return parseFloat(discountedPrice.toFixed(2));
 }
 
 module.exports = calculateDiscountedPrice;
